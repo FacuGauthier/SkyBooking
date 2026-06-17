03cab4fc648fc96e72e153336fd7905b0b4d86e9
FacuGauthier <facundosgauthier@gmail.com>
2026-06-16 21:12:28 -0300

    added: entity Passage & enum TravelClass (incomplete)

---
+++ src/main/java/com/skybooking/backend/models/Passage.java
@@ -0,0 +1,46 @@
+package com.skybooking.backend.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "passages")
+public class Passage {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String ticketNumber;
+
+    private String seatNumber;
+
+    private TravelClass travelClass;
+
+    @Column(nullable = false, precision = 19, scale = 2)
+    private BigDecimal price;
+
+    @ManyToOne
+    @JoinColumn(name = "passenger_id")
+    private Passenger passenger;
+
+    @ManyToOne
+    @JoinColumn(name = "flight_id")
+    private Flight flight;
+
+    @ManyToOne
+    @JoinColumn(name = "booking_id")
+    private Booking booking;
+
+    @OneToMany(mappedBy = "passages")
+    private List<Luggage> luggage;
+}
---
+++ src/main/java/com/skybooking/backend/models/TravelClass.java
@@ -0,0 +1,4 @@
+package com.skybooking.backend.models;
+
+public enum TravelClass {
+}
