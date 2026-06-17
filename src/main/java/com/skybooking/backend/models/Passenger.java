46191ea9860fe7dbb02effc58755c08007e1189c
FacuGauthier <facundosgauthier@gmail.com>
2026-06-16 21:05:08 -0300

    added: entity Passenger

---
+++ src/main/java/com/skybooking/backend/models/Passenger.java
@@ -0,0 +1,34 @@
+package com.skybooking.backend.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "passengers")
+public class Passenger {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+
+    @Column(unique = true)
+    private String DNI;
+
+    private String nationality;
+
+    private LocalDate birthDate;
+
+    private String gender;
+}
