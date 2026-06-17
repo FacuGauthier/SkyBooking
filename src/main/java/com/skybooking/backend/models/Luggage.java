63f9ce8cb4d1fcd52eb837dc3a58adbf74039415
FacuGauthier <facundosgauthier@gmail.com>
2026-06-16 21:24:41 -0300

    added: entity Luggage & enum LuggageType (incomplete)

---
+++ src/main/java/com/skybooking/backend/models/Luggage.java
@@ -0,0 +1,38 @@
+package com.skybooking.backend.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "luggage")
+public class Luggage {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private LuggageType type;
+
+    private Double weightKg;
+    private Double heightCm;
+    private Double widthCm;
+    private Double lengthCm;
+
+    private String description;
+
+    @Column(nullable = false, precision = 12, scale = 2)
+    private BigDecimal additionalCost;
+
+    @ManyToOne
+    @JoinColumn(name = "passage_id")
+    private Passage passage;
+}
