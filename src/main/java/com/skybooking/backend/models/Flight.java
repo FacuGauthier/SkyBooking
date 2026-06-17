f62389ed0542259a47bc860c8e379df35652936d
FacuGauthier <facundosgauthier@gmail.com>
2026-06-16 21:21:50 -0300

    added: entity Flight & enum FlightStatus (incomplete)

---
+++ src/main/java/com/skybooking/backend/models/Flight.java
@@ -0,0 +1,44 @@
+package com.skybooking.backend.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "flights")
+public class Flight {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String flightNumber;
+
+    private Airport originAirport;
+    private Airport destinationAirport;
+
+    private LocalDateTime departureTime;
+    private LocalDateTime arrivalTime;
+
+    private FlightStatus status;
+
+    @Column(nullable = false, precision = 19, scale = 2)
+    private BigDecimal basePrice;
+
+    @ManyToOne
+    @JoinColumn(name = "plane_id")
+    private Plane plane;
+
+    @ManyToOne
+    @JoinColumn(name = "airline_id")
+    private Airline airline;
+}
