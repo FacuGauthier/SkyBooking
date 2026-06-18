f1a76db28321ee1edb2568852553ebeeef6d135c
FacuGauthier <facundosgauthier@gmail.com>
2026-06-18 02:36:17 -0300

    fix: entity Client

--- src/main/java/com/skybooking/backend/models/Client.java
+++ src/main/java/com/skybooking/backend/models/Client.java
@@ -19,17 +19,22 @@ public class Client {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String firstName;
+
+    @Column(nullable = false)
     private String lastName;
 
-    @Column(unique = true)
+    @Column(unique = true, nullable = false)
     private String email;
 
+    @Column(nullable = false)
     private String passwordHash;
 
     @Column(unique = true)
     private String phone;
 
     @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private Role role;
 }
