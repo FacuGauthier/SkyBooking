928899263f1d72bd83a27da95046501ae8ec91ed
FacuGauthier <facundosgauthier@gmail.com>
2026-06-24 20:13:15 -0300

    added: dto ChangePasswordRequest

---
+++ src/main/java/com/skybooking/backend/dtos/auth/ChangePasswordRequest.java
@@ -0,0 +1,9 @@
+package com.skybooking.backend.dtos.auth;
+
+import jakarta.validation.constraints.NotBlank;
+
+public record ChangePasswordRequest(
+        @NotBlank String currentPassword,
+        @NotBlank String newPassword
+) {
+}
