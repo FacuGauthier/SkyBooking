e9a51f37dd30094d01330e8fbc59a4f90cc59a08
FacuGauthier <facundosgauthier@gmail.com>
2026-06-27 20:04:02 -0300

    fix: typo in JwtAuthenticationFilter

--- src/main/java/com/skybooking/backend/security/JwtAuthenticationFilter.java
+++ src/main/java/com/skybooking/backend/security/JwtAuthenticationFilter.java
@@ -5,7 +5,6 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-// IMPORTANTE: Cambiamos el import de Spring por el de JSpecify
 import org.jspecify.annotations.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
