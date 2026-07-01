bdcecd61fc881ec597ab38bd4da4afdbc3ab7784
FacuGauthier <facundosgauthier@gmail.com>
2026-06-30 21:17:58 -0300

    added: necessary imports

--- src/main/java/com/skybooking/backend/repositories/PassageRepository.java
+++ src/main/java/com/skybooking/backend/repositories/PassageRepository.java
@@ -1,6 +1,8 @@
 package com.skybooking.backend.repositories;
 
 import com.skybooking.backend.models.Passage;
+import com.skybooking.backend.models.enums.BookingStatus;
+import com.skybooking.backend.models.enums.TravelClass;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
