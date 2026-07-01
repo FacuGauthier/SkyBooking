8378211b9f7e1f07f21367f931dce295d969c48d
FacuGauthier <facundosgauthier@gmail.com>
2026-06-30 21:17:45 -0300

    added: complete query and method to the FlightRepository

--- src/main/java/com/skybooking/backend/repositories/FlightRepository.java
+++ src/main/java/com/skybooking/backend/repositories/FlightRepository.java
@@ -11,4 +11,23 @@ import java.util.List;
 
 @Repository
 public interface FlightRepository extends JpaRepository<Flight, Long> {
+    @Query("SELECT f FROM Flight f " +
+            "WHERE f.status <> com.skybooking.backend.models.enums.FlightStatus.CANCELLED " +
+            "AND (:originCode IS NULL OR f.originAirport.iataCode = :originCode) " +
+            "AND (:destinationCode IS NULL OR f.destinationAirport.iataCode = :destinationCode) " +
+            "AND (:startDateTime IS NULL OR f.departureTime >= :startDateTime) " +
+            "AND (:endDateTime IS NULL OR f.departureTime <= :endDateTime) " +
+            "AND (:airlineName IS NULL OR LOWER(f.plane.airline.name) LIKE LOWER(CONCAT('%', :airlineName, '%'))) " +
+            "AND (:maxPrice IS NULL OR " +
+            "     (:travelClass = 'BUSINESS' AND f.businessPrice <= :maxPrice) OR " +
+            "     ((:travelClass IS NULL OR :travelClass = 'ECONOMY') AND f.economyPrice <= :maxPrice))")
+    List<Flight> searchFlightsDynamic(
+            @Param("originCode") String originCode,
+            @Param("destinationCode") String destinationCode,
+            @Param("startDateTime") LocalDateTime startDateTime,
+            @Param("endDateTime") LocalDateTime endDateTime,
+            @Param("airlineName") String airlineName,
+            @Param("maxPrice") Double maxPrice,
+            @Param("travelClass") String travelClass
+    );
 }
