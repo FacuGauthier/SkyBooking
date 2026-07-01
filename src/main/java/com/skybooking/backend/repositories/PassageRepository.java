691b2c4ad04431e5ac5d78a24d9f8f640e5c0473
FacuGauthier <facundosgauthier@gmail.com>
2026-06-30 22:57:12 -0300

    added: necessary imports

--- src/main/java/com/skybooking/backend/repositories/PassageRepository.java
+++ src/main/java/com/skybooking/backend/repositories/PassageRepository.java
@@ -8,6 +8,8 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PassageRepository extends JpaRepository<Passage, Long> {
     int countByFlightIdAndTravelClassAndBookingStatusNot(Long flightId, TravelClass travelClass, BookingStatus bookingStatus);
